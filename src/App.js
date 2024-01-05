af8121b700ef99f61357e5da9d6b104c2e23a5de
sCodeu <sachindu99u@gmail.com>
2024-01-05 09:52:52 +0530

    create routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dashbordd from "./scenes/dashbord/Dashbordd";
+import Exercises from "./scenes/exercises/Exercises";
+import Goals from "./scenes/goals/Goals";
+import Profile from "./scenes/profile/Profile";
+import Workout_plan from "./scenes/workout_plan/Workout_plan";
 
 function App() {
   return (
@@ -7,6 +11,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashbordd />} />
+          <Route path="/exercises" element={<Exercises/>}/>
+          <Route path="/goals" element={<Goals/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/workout_plan" element={<Workout_plan/>}/>
         </Routes>
       </BrowserRouter>
     </div>
