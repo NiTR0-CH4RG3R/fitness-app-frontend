03bc7f14b0c37a7f4f30b84e1dde34e4f440e1b9
sCodeu <sachindu99u@gmail.com>
2024-01-05 12:05:55 +0530

    intiate side bar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import{ProSidebar,Menu,MenuItem} from 'react-pro-sidebar';
+import { Box,IconButton,Typography,useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 
