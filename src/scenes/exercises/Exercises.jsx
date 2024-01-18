5395318413b4a38854036bac833dfb5080be74ac
sCodeu <sachindu99u@gmail.com>
2024-01-18 10:38:17 +0530

    Implemented card componet inside the grid

--- src/scenes/exercises/Exercises.jsx
+++ src/scenes/exercises/Exercises.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import ExercisesCard from "../../Components/Card/ExercisesCard";
+
+
 
 export default function Exercises() {
   return (
     <div>
-      exercises
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={3}>
+           <ExercisesCard
+           imagelink="link"
+           title="Exercises1"
+           content="Add here content"
+           
+           />
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
