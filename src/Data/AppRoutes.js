cbf4859cd599fafd2bba446c23a75ad6adc78566
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-20 18:44:50 +0530

    All the routes and their elements are here

---
+++ src/Data/AppRoutes.js
@@ -0,0 +1,14 @@
+import Dashboard from "../Pages/Dashboard/Dashboard";
+import ExerciseList from "../Pages/Exercise/ExerciseList";
+import GoalList from "../Pages/Goal/GoalList";
+import WorkoutPlanList from "../Pages/WorkoutPlan/WorkoutPlanList";
+
+export const AppRoutes = {
+    dashboard: { path: '/dashboard', component: <Dashboard /> },
+    login: { path: '/login', component: (<></>) },
+    exercise_list: { path: '/exercise/list', component: <ExerciseList /> },
+    exercise: { path: '/exercise/id', component: (<></>) },
+    workout_plan: { path: '/workout/list', component: <WorkoutPlanList /> },
+    goal: { path: '/goal/list', component: <GoalList /> },
+    profile: { path: '/profile', component: (<></>) },
+}
