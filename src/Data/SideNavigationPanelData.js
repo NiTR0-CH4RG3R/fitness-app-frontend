828b3b376e2b3b506a7f972c7f53205feb9d8a2f
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-01-20 18:45:23 +0530

    Side navigation panel data is in this file

---
+++ src/Data/SideNavigationPanelData.js
@@ -0,0 +1,29 @@
+import * as MuiIcons from '@mui/icons-material';
+
+import { AppRoutes } from './AppRoutes';
+
+export const SideNavigationPanelData = {
+    width: 300,
+    items: [
+        {
+            title: { primary: 'Dashboard', secondary: 'You can find quick link to everythin here!' },
+            icon: <MuiIcons.Dashboard />,
+            path: AppRoutes.dashboard.path,
+        },
+        {
+            title: { primary: 'Exercises', secondary: 'We have more than 100+ exercises' },
+            icon: <MuiIcons.FitnessCenter />,
+            path: AppRoutes.exercise_list.path,
+        },
+        {
+            title: { primary: 'Workout Plan', secondary: 'Create your own workout plan, or follow others' },
+            icon: <MuiIcons.ArrowForward />,
+            path: AppRoutes.workout_plan.path,
+        },
+        {
+            title: { primary: 'Goals', secondary: 'Create your own customized goals!' },
+            icon: <MuiIcons.Flag />,
+            path: AppRoutes.goal.path,
+        },
+    ]
+}
