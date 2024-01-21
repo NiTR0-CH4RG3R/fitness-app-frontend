7f734e1aea6c209d8dbc1295cd127f237847749e
sCodeu <sachindu99u@gmail.com>
2024-01-21 12:31:11 +0530

    Added Youtube player to the Details page

--- src/scenes/exercises/Details.jsx
+++ src/scenes/exercises/Details.jsx
@@ -1,31 +1,31 @@
 import React from "react";
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
+import YouTube from "react-youtube";
 
 export default function Details() {
   return (
     <div>
       <div>
-      <Box sx={{ flexGrow: 1 }}>
-        <Grid container spacing={2}>
-        <Grid item xs={12} >
-            photo slide
-
-        </Grid>
-        <Grid item xs={12} >
-            Description
-        </Grid>
-
-        </Grid>
-        
-      </Box>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              photo slide
+            </Grid>
+            <Grid item xs={12}>
+              Description
+            </Grid>
+          </Grid>
+        </Box>
       </div>
-      <div
-      style={{display:"flex",justifyContent:"center",}}
-      >
-        hello
-
+      <div style={{ display: "flex", justifyContent: "center" }}>
+      <YouTube
+          videoId="AWl3WBAU4h8"
+          opts={{ width: "560", height: "315" }}
+        />
       </div>
+      
+      
     </div>
   );
 }
