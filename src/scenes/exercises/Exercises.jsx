ff52dcd9ddd9ddcb4409d99974fcc2627b4875b6
sCodeu <sachindu99u@gmail.com>
2024-01-21 11:48:14 +0530

    Added onclick

--- src/scenes/exercises/Exercises.jsx
+++ src/scenes/exercises/Exercises.jsx
@@ -3,28 +3,27 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 import ExercisesCard from "../../Components/Card/ExercisesCard";
 
-
-
 export default function Exercises() {
+  const [data, setData] = React.useState([]);
+  React.useEffect(() => {
+    getDataFromApi();
+  }, []);
 
-  const [data,setData]=React.useState({
-    imagelink:"",
-    title:"",
-    content:""
-  });
-
+  function getDataFromApi() {}
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
         <Grid container spacing={2}>
-          <Grid item xs={3}>
-           <ExercisesCard
-           imagelink={data.imagelink}
-           title={data.title}
-           content={data.content}
-           
-           />
-          </Grid>
+          {data.map((exercise, index) => (
+            <Grid item xs={3} key={index}>
+              <ExercisesCard
+                id={exercise.id}
+                imagelink={exercise.imagelink}
+                title={exercise.title}
+                content={exercise.content}
+              />
+            </Grid>
+          ))}
         </Grid>
       </Box>
     </div>
