0d9ee49119356ac61629e9cd0737785ba9a41def
sCodeu <sachindu99u@gmail.com>
2024-01-21 23:30:20 +0530

    Added Comment section

--- src/scenes/exercises/Details.jsx
+++ src/scenes/exercises/Details.jsx
@@ -6,7 +6,26 @@ import Carousel from "react-material-ui-carousel";
 import Rating from '@mui/material/Rating';
 import Typography from '@mui/material/Typography';
 
+import Button from '@mui/joy/Button';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Textarea from '@mui/joy/Textarea';
+import IconButton from '@mui/joy/IconButton';
+import Menu from '@mui/joy/Menu';
+import MenuItem from '@mui/joy/MenuItem';
+import ListItemDecorator from '@mui/joy/ListItemDecorator';
+import FormatBold from '@mui/icons-material/FormatBold';
+import FormatItalic from '@mui/icons-material/FormatItalic';
+import KeyboardArrowDown from '@mui/icons-material/KeyboardArrowDown';
+import Check from '@mui/icons-material/Check';
+
+
+
 export default function Details() {
+
+  const [italic, setItalic] = React.useState(false);
+  const [fontWeight, setFontWeight] = React.useState('normal');
+  const [anchorEl, setAnchorEl] = React.useState(null);
 
   const [value, setValue] = React.useState(1);
 
@@ -55,7 +74,77 @@ export default function Details() {
       <div style={{ display: "flex", justifyContent: "center" }}>
         <YouTube videoId="AWl3WBAU4h8" opts={{ width: "560", height: "315" }} />
       </div>
-      <div style={{ display: "flex", justifyContent: "center" ,padding:"2rem"}}>
+      <div style={{ display: "flex", justifyContent: "center" ,padding:"2rem 0 0 0"}}>
+      <FormControl>
+      <FormLabel>Your comment</FormLabel>
+      <Textarea
+        placeholder="Type something here…"
+        minRows={3}
+        endDecorator={
+          <Box
+            sx={{
+              display: 'flex',
+              gap: 'var(--Textarea-paddingBlock)',
+              pt: 'var(--Textarea-paddingBlock)',
+              borderTop: '1px solid',
+              borderColor: 'divider',
+              flex: 'auto',
+            }}
+          >
+            <IconButton
+              variant="plain"
+              color="neutral"
+              onClick={(event) => setAnchorEl(event.currentTarget)}
+            >
+              <FormatBold />
+              <KeyboardArrowDown fontSize="md" />
+            </IconButton>
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={() => setAnchorEl(null)}
+              size="sm"
+              placement="bottom-start"
+              sx={{ '--ListItemDecorator-size': '24px' }}
+            >
+              {['200', 'normal', 'bold'].map((weight) => (
+                <MenuItem
+                  key={weight}
+                  selected={fontWeight === weight}
+                  onClick={() => {
+                    setFontWeight(weight);
+                    setAnchorEl(null);
+                  }}
+                  sx={{ fontWeight: weight }}
+                >
+                  <ListItemDecorator>
+                    {fontWeight === weight && <Check fontSize="sm" />}
+                  </ListItemDecorator>
+                  {weight === '200' ? 'lighter' : weight}
+                </MenuItem>
+              ))}
+            </Menu>
+            <IconButton
+              variant={italic ? 'soft' : 'plain'}
+              color={italic ? 'primary' : 'neutral'}
+              aria-pressed={italic}
+              onClick={() => setItalic((bool) => !bool)}
+            >
+              <FormatItalic />
+            </IconButton>
+            <Button sx={{ ml: 'auto' }}>Send</Button>
+          </Box>
+        }
+        sx={{
+          minWidth: 300,
+          fontWeight,
+          fontStyle: italic ? 'italic' : 'initial',
+        }}
+      />
+    </FormControl>
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" ,padding:"1rem"}}>
+        
       <Box
       sx={{
         '& > legend': { mt: 2 },
