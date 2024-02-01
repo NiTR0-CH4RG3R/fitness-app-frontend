ffb22e4a37cc42ebe5c10ba21016100dde19e572
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-01 13:04:38 +0530

    Migrated Exercise page from the CodeSU branch

--- src/Data/AppRoutes.js
+++ src/Data/AppRoutes.js
@@ -1,5 +1,6 @@
 import Dashboard from "../Pages/Dashboard/Dashboard";
 import ExerciseList from "../Pages/Exercise/ExerciseList";
+import Exercise from "../Pages/Exercise/Exercise";
 import GoalList from "../Pages/Goal/GoalList";
 import WorkoutPlanList from "../Pages/WorkoutPlan/WorkoutPlanList";
 
@@ -7,7 +8,7 @@ export const AppRoutes = {
     dashboard: { path: '/dashboard', component: <Dashboard /> },
     login: { path: '/login', component: (<></>) },
     exercise_list: { path: '/exercise/list', component: <ExerciseList /> },
-    exercise: { path: '/exercise/id', component: (<></>) },
+    exercise: { path: '/exercise/id', component: <Exercise /> },
     workout_plan: { path: '/workout/list', component: <WorkoutPlanList /> },
     goal: { path: '/goal/list', component: <GoalList /> },
     profile: { path: '/profile', component: (<></>) },
