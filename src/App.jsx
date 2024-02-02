993f48d39e6bc58524efe368692d1c43b9ce7da1
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-02 13:39:01 +0530

    Moved themeing stuff to the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
+import { UserContextProvider } from './Context/UserContext';
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import { ColorModeContext, useMode } from "./theme";
 import Layout from './Layout';
 import { AppRoutes } from './Data/AppRoutes';
 import Missing from './Pages/Missing/Missing';
+import Login from './Pages/Login/Login';
 
 
 export default function App() {
@@ -10,15 +16,24 @@ export default function App() {
     routes.push(AppRoutes[name]);
   });
 
-  return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        {routes.map((route) => (
-          <Route key={route} path={route.path} element={route.component} />
-        ))}
+  const [theme, colorMode] = useMode();
 
-        <Route path='*' element={<Missing />} />
-      </Route>
-    </Routes>
+  return (
+    <UserContextProvider>
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={<Login />} />
+            <Route path='/*' element={<Layout />}>
+              {routes.map((route) => (
+                <Route key={route} path={route.path.substr(1)} element={route.component} />
+              ))}
+              <Route path='*' element={<Missing />} />
+            </Route>
+          </Routes>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+    </UserContextProvider>
   );
 }
