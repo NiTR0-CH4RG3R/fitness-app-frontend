5bca582ea410b706998ee45ffe5bf89ab2e12c4c
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-02 13:38:47 +0530

    Did some small changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,38 +1,31 @@
 import { Box } from '@mui/material';
-import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from "@mui/material/CssBaseline";
+
 import { Outlet } from 'react-router-dom';
-import { ColorModeContext, useMode } from "./theme";
 import TopBar from './Components/TopBar';
 import SideNavigationPanel from './Components/SideNavigationPanel';
 import { SideNavigationPanelData } from './Data/SideNavigationPanelData';
 
 export default function Layout() {
-
-    const [theme, colorMode] = useMode();
-
     return (
-        <ColorModeContext.Provider value={colorMode}>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Box className='app' sx={{ display: 'flex' }}>
-                    <main className='content' style={{ display: 'flex' }}>
-                        <SideNavigationPanel SideNavigationPanelMenuItems={SideNavigationPanelData.items} drawerWidth={SideNavigationPanelData.width} toolbarHeight={SideNavigationPanelData.toolbarHeight} />
-                        <TopBar drawerWidth={SideNavigationPanelData.width} topBarHeight={SideNavigationPanelData.toolbarHeight} />
-                        <Box
-                            display='flex'
-                            width={`calc(100% - ${SideNavigationPanelData.width}px)`}
-                            // ml={`${SideNavigationPanelData.width}px`}
-                            height={`calc(100% - ${SideNavigationPanelData.toolbarHeight}px)`}
-                            mt={`${SideNavigationPanelData.toolbarHeight}px`}
-                            justifyContent='center'
-                            alignItems='center'
-                        >
-                            <Outlet />
-                        </Box>
-                    </main>
+        <Box className='app' sx={{ display: 'flex' }}>
+            <main className='content' style={{ display: 'flex' }}>
+                <SideNavigationPanel SideNavigationPanelMenuItems={SideNavigationPanelData.items} drawerWidth={SideNavigationPanelData.width} toolbarHeight={SideNavigationPanelData.toolbarHeight} />
+
+                <Box display='flex' flexDirection='column' width='100%'>
+                    <TopBar drawerWidth={SideNavigationPanelData.width} topBarHeight={SideNavigationPanelData.toolbarHeight} />
+                    <Box
+                        display='flex'
+                        flexDirection='column'
+                        width='100%'
+                        height={`calc(100% - ${SideNavigationPanelData.toolbarHeight}px)`}
+                        mt={`${SideNavigationPanelData.toolbarHeight}px`}
+                        justifyContent='center'
+                        alignItems='center'
+                    >
+                        <Outlet />
+                    </Box>
                 </Box>
-            </ThemeProvider>
-        </ColorModeContext.Provider>
+            </main>
+        </Box>
     );
 }
