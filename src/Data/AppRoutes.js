a1b1595a7da678cdca6d36a54f6a734f53bf544d
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2024-02-03 08:28:06 +0530

    Added register page to the routes

--- src/Data/AppRoutes.js
+++ src/Data/AppRoutes.js
@@ -3,9 +3,12 @@ import ExerciseList from "../Pages/Exercise/ExerciseList";
 import Exercise from "../Pages/Exercise/Exercise";
 import GoalList from "../Pages/Goal/GoalList";
 import WorkoutPlanList from "../Pages/WorkoutPlan/WorkoutPlanList";
+import Register from "../Pages/Login/Register";
+import RegisterGenerated from "../Pages/Login/RegisterGenerated";
 
 export const AppRoutes = {
     dashboard: { path: '/dashboard', component: <Dashboard /> },
+    dashboard: { path: '/register', component: <RegisterGenerated /> },
     exercise_list: { path: '/exercise/list', component: <ExerciseList /> },
     exercise: { path: '/exercise/id', component: <Exercise /> },
     workout_plan: { path: '/workout/list', component: <WorkoutPlanList /> },
